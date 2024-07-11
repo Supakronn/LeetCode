732b9637fc438a4a171c6c9c95d7895858bc478e
Supakronn <141012759+Supakronn@users.noreply.github.com>
2024-07-11 22:04:07 +0700

    sum 0199,0216,0236,0547,0994,2336,2542

---
+++ 2336SmallestNuberInInfSet_P.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
+class SmallestInfiniteSet {
+private: 
+    vector<bool> flag;
+    int i = 1;
+public:
+    SmallestInfiniteSet() : flag(1010, false) {}
+
+    int popSmallest() {
+        int prev = i; i++;
+        while (flag[i]) { i++; }
+        return prev;
+    }
+
+    void addBack(int num) {
+        flag[num] = false;
+        i = min(num, i);
+    }
+};
+
+int main() {
+    //it must create each command by myself so I am lazy run this class in leet code
+}
